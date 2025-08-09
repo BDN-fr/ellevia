d7be46361eda5c3961db550714599d3299aec015
BDN_fr <98167323+BDN-fr@users.noreply.github.com>
2025-08-09 22:22:12 +0200

    Make site good for online

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/images/logo-small.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<script defer src="https://analytics.bdn-fr.xyz/script.js" data-website-id="d7a48764-bd4d-4ffc-bbe4-bea51d5f8b40"></script>
 </svelte:head>
 
 {@render children?.()}
-<!-- Footer with credits and more -->
+<!-- Todo: Footer with credits and more -->
